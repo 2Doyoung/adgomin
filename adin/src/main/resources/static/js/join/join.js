04302f084761d4acf65287901bce17224a715feb
riverwings <candylove95@naver.com>
2023-01-27 13:41:49 +0900

    xhr 함수

--- adin/src/main/resources/static/js/join/join.js
+++ adin/src/main/resources/static/js/join/join.js
@@ -1,3 +1,6 @@
+/**
+ 전역변수
+ */
 const form = window.document.getElementById('form');
 
 const Warning = {
@@ -9,6 +12,64 @@ const Warning = {
         form.querySelector('[rel="warning"]').classList.remove('visible');
     }
 };
+
+/**
+ 이벤트 함수
+ */
+loginBtn.addEventListener('click', () => {
+    loginCover.style.display = 'flex';
+});
+
+if (closeBtn !== null) {
+    closeBtn.addEventListener('click', () => {
+        loginCover.style.display = 'none';
+    });
+}
+
+signupBtn.addEventListener('click', () => {
+    window.location.href = '/join'
+});
+
+/**
+ 사용자정의 함수
+ */
+
+/**
+ xhr 성공시
+ */
+let successXhr = (responseObject, flag) => {
+
+}
+
+/**
+ xhr 공통 함수
+ */
+let xhr = (url, formData, method, flag) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                const responseObject = JSON.parse(xhr.responseText);
+                switch (responseObject['result']) {
+                    case 'success':
+                        successXhr(responseObject, flag);
+                        break;
+                    default:
+                        alert('오류가 발생했습니다.');
+                }
+            } else {
+                alert('서버와 통신하지 못하였습니다.');
+            }
+        }
+    }
+    xhr.send(formData);
+}
+
+
+
+
 
 
 form.querySelector('[rel="nextBtn"]').addEventListener('click', () => {
