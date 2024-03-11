89ae66fd17a0a9c6fa5f84881100eb4bd4ccabaf
2Doyoung <dlehdud1653@kakao.com>
2024-03-12 08:31:17 +0900

    commit

--- adgomin/src/main/java/com/adgomin/manage/controller/ManageController.java
+++ adgomin/src/main/java/com/adgomin/manage/controller/ManageController.java
@@ -5,11 +5,22 @@ import com.adgomin.manage.service.ManageService;
 import com.adgomin.media.entity.MediaIntroduceEntity;
 import com.adgomin.media.entity.MediaRegisterEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller(value = "com.adgomin.manage.controller.ManageController")
 public class ManageController {
@@ -27,9 +38,37 @@ public class ManageController {
             modelAndView =  new ModelAndView("manage/media");
             if(joinVO != null) {
                 MediaRegisterEntity[] allMediaRegister = this.manageService.allMediaRegister(joinVO.getEmail());
+
+                modelAndView.addObject("allMediaRegister", allMediaRegister);
             }
         }
 
         return  modelAndView;
+    }
+
+    @GetMapping("/manage/thumbnail/image")
+    public ResponseEntity<Resource> mainThumbnailImage(@RequestParam(value = "thumbnailImgNm") String thumbnailImgNm, @RequestParam(value = "thumbnailImgFilePath") String thumbnailImgFilePath) {
+        String filePath = null;
+
+        if("".equals(thumbnailImgNm) || "".equals(thumbnailImgFilePath)) {
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(classLoader.getResource("static/images/media-register-thumbnail.png").getFile());
+            filePath = file.getAbsolutePath();
+        } else {
+            filePath = thumbnailImgFilePath + "/" + thumbnailImgNm;
+        }
+
+        Resource resource = new FileSystemResource(filePath);
+
+        HttpHeaders header = new HttpHeaders();
+        Path path = null;
+        try {
+            path = Paths.get(filePath);
+            header.add("Content-type", Files.probeContentType(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
     }
 }
